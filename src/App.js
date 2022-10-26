6b00a63c1800a04ab353526cf7c2090fbef89ef9
yazshrilld <yazshrilld@gmail.com>
2022-10-26 10:50:59 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+   <>
+    <div className='bg-black text-white text-3xl'>
+    Hello, Yazid!
+    </div>
+   </>
+  );
+}
+
+export default App;
